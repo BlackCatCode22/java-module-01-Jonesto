9195e8bb2a11f2d2f29fdc1464154428c5176802
Jonesto <105900518+Jonesto@users.noreply.github.com>
2025-01-26 00:15:56 -0800

    TempConverter

---
+++ TempConverter/src/TempConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TempConverter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter the temperature in Fahrenheit: ");
+
+
+        double fahrenheit = scanner.nextDouble();
+
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+
+        System.out.printf("%.2f°F is equal to %.2f°C%n", fahrenheit, celsius);
+
+
+        scanner.close();
+    }
+}
