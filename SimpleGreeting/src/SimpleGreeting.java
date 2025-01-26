96b0dd099dd60f397f2aa936446bb2249b1b8e99
Jonesto <105900518+Jonesto@users.noreply.github.com>
2025-01-25 23:54:08 -0800

    SimpleGreeting

---
+++ SimpleGreeting/src/SimpleGreeting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SimpleGreeting {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Hello, " + name + "! It's nice to meet you.");
+
+      
+        scanner.close();
+    }
+}
