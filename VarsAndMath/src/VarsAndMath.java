6fd9164d5d17201f397aa5ecbc5aabac2c25348d
Jonesto <105900518+Jonesto@users.noreply.github.com>
2025-01-25 23:47:51 -0800

    Update VarsAndMath.java

--- VarsAndMath/src/VarsAndMath.java
+++ VarsAndMath/src/VarsAndMath.java
@@ -1,26 +1,37 @@
 public class VarsAndMath {
     public static void main(String[] args) {
 
-        int num1 = 25;
-        int num2 = 35;
-        double pi = 3.14159;
-        boolean isJavaFun = true;
+        String studentName = "John Doe";
+        int mathGrade = 95;
+        int scienceGrade = 88;
+        int englishGrade = 98;
 
 
-        int sum = num1 + num2;
-        int difference = num2 - num1;
-        int product = num1 * num2;
-        double division = (double) num2 / num1; 
+        double averageGrade = (mathGrade + scienceGrade + englishGrade) / 3.0;
 
 
-        String message = "Is learning Java fun? " + isJavaFun;
-        String arithmeticResults = "Sum: " + sum + ", Difference: " + difference +
-                ", Product: " + product + ", Division: " + division;
+        String letterGrade;
+        if (averageGrade >= 90) {
+            letterGrade = "A";
+        } else if (averageGrade >= 80) {
+            letterGrade = "B";
+        } else if (averageGrade >= 70) {
+            letterGrade = "C";
+        } else if (averageGrade >= 60) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
 
 
-        System.out.println("Hello, Java!");
-        System.out.println("Value of pi: " + pi);
-        System.out.println(message);
-        System.out.println(arithmeticResults);
+        String report = "Student Name: " + studentName + "\n" +
+                "Math Grade: " + mathGrade + "\n" +
+                "Science Grade: " + scienceGrade + "\n" +
+                "English Grade: " + englishGrade + "\n" +
+                "Average Grade: " + averageGrade + "\n" +
+                "Letter Grade: " + letterGrade;
+
+        // Print the report
+        System.out.println(report);
     }
 }
